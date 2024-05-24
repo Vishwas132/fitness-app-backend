9ffddeebc499b148f4b5cf68efbce678838b76f9
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2024-05-24 05:47:53 +0530

    Add seed data

--- config/development.js
+++ config/development.js
@@ -2,7 +2,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   db: {
     host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT || 5433,
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "root",
     database: process.env.DB_NAME || "fitness_app",
